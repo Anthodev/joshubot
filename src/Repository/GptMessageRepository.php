f35b534682e63e42f9038f1427a61d42d070693a
Cédric Anthony <Anthodev@users.noreply.github.com>
2023-11-22 23:36:22 +0100

    feat(improvement): added history support + delete history (#11)

--- src/Repository/GptMessageRepository.php
+++ src/Repository/GptMessageRepository.php
@@ -21,28 +21,12 @@ class GptMessageRepository extends ServiceEntityRepository
         parent::__construct($registry, GptMessage::class);
     }
 
-//    /**
-//     * @return GptMessage[] Returns an array of GptMessage objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('g')
-//            ->andWhere('g.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('g.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?GptMessage
-//    {
-//        return $this->createQueryBuilder('g')
-//            ->andWhere('g.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function deleteAll(): void
+    {
+        $this->createQueryBuilder('g')
+            ->delete()
+            ->getQuery()
+            ->execute()
+        ;
+    }
 }
